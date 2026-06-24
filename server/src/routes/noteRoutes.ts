9c4b1d39933f49ed914ab0093b32fd963e6dda0b
Sissighn <99798403+Sissighn@users.noreply.github.com>
2026-06-25 00:15:58 +0200

    feat(note): add updateNote route to noteRoutes

--- server/src/routes/noteRoutes.ts
+++ server/src/routes/noteRoutes.ts
@@ -6,6 +6,7 @@ const noteController = new NoteController();
 
 router.post("/", (req, res) => noteController.addNote(req, res));
 router.get("/", (req, res) => noteController.getAllNotes(req, res));
+router.patch("/:id", (req, res) => noteController.updateNote(req, res));
 router.delete("/:id", (req, res) => noteController.deleteNote(req, res));
 router.get("/:id/download", (req, res) =>
   noteController.downloadNote(req, res),
